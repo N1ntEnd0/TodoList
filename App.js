1104a6f7fbd91b4be3fc0b2e9a49ff5ef8a623ae
N1ntEnd0 <48650514+N1ntEnd0@users.noreply.github.com>
2021-12-31 19:04:13 +0300

    add src

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import {Button, FlatList, SafeAreaView, Text, TextInput, View} from 'react-native';
-import MainPage from './pages/MainPage/MainPage';
+import MainPage from './src/pages/MainPage/MainPage';
 import { Toast } from 'react-native-toast-message/lib/src/Toast';
 import { Provider } from 'react-redux';
-import { store } from './store/index';
+import { store } from './src/store/index';
 
 const App = () => {
 
